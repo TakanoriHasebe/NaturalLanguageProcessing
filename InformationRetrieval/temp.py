1aa29f1c3a918a94089e2d1681a965d76096c954
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-01-30 14:18:21 +0900

    cosine normalization is added

--- InformationRetrieval/temp.py
+++ InformationRetrieval/temp.py
@@ -9,27 +9,27 @@ Created on Thu Dec 22 17:46:03 2016
 import nltk
 import math
 import InformationRetrieval as ir
+import codecs
+import MeCab
 
-
+# 各文章から名詞を抜き出してきたもの
 docs = [
-    ["肉", "寿司", "ピザ", "ラーメン", "肉"],
-    ["肉","肉","肉","肉"],
-    ["寿司","天麩羅","そば","ラーメン","和食", "中心"],
-    ["スイーツ","野菜","スイーツ","野菜", "交互"]
+    ["肉", "寿司", "ピザ", "ラーメン", "肉"], # 1番目の文章
+    ["肉","肉","肉","肉"], # 2番目の文章
+    ["寿司","天麩羅","そば","ラーメン","和食", "中心"], # 3番目の文章
+    ["スイーツ","野菜","スイーツ","野菜", "交互"] # 4番目の文章
 ]
 
-#tfidfの表示
+# docsの表示
+# print(docs)
+
+# tfidfの表示
 tfidf_result = ir.tfidf(docs)
+
+# 代入したdocsのtfidfの表示
 for l in tfidf_result:
     
     print(l)
-print(' ')
-
-ridf_result = ir.ridf(docs)
-for r in ridf_result:
-    
-    print(r)
-
 
 
 
--- InformationRetrieval/temp_math.py
+++ InformationRetrieval/temp_math.py
@@ -11,6 +11,7 @@ math
 """
 
 import math
+import numpy as np
 
 print(math.log2(2))
 
@@ -24,4 +25,31 @@ print(math.exp(2))
 
 #print(math.log2(1 - math.exp(2)))
 
-print(math.log2(-5))
+#print(math.log2(-5))
+
+# value_array = value_array / np.linalg.norm(value_array)
+
+arr = np.array([75, 82])
+res = arr / np.linalg.norm(arr)
+print(arr)
+print(res)
+
+# 辞書の要素をコサイン正規化する
+# 対象の辞書
+d = {"yamada":75, "endou":82}
+# 辞書の要素を抜き出し, コサイン正規化し, 新しいリストの作成
+value_list = list(d.values()) / np.linalg.norm(list(d.values()))
+# 辞書から単語を抜き出し, 単語のリストを作成
+word_list = list(d.keys())
+print(word_list)
+# 辞書にコサイン類似度で算出した値を入れる
+for i in range(len(word_list)):
+    
+    d[word_list[i]] = value_list[i]
+# コサイン正規化された
+print("コサイン正規化された辞書の表示: "+str(d))
+
+
+
+
+
